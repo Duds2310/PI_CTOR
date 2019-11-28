7ab119bcd15c14607a55d5ef421039761e4a73e9
Your Name <you@example.com>
2019-11-27 21:38:10 -0300

    correção dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,6 +15,21 @@ $repoReceita = new RepositorioReceita();
 
 $retornoReceita = $repoReceita->somarReceita();
 
+$retornoTotal = $retornoReceita->getValor() - $retornoDespesa->getValor();
+
+
+$retornoCor = null;
+
+if ($retornoReceita->getValor() < $retornoDespesa->getValor()) {
+    
+    $retornoCor = "danger";
+       
+} else {
+    $retornoCor = "success";
+}
+
+
+
 
 ?>
 <!-- Breadcrumbs-->
@@ -29,7 +44,7 @@ $retornoReceita = $repoReceita->somarReceita();
 		<div class="card text-white bg-primary o-hidden h-100">
 			<div class="card-body">
 				<div class="card-body-icon">
-					<i class="fas fa-fw fa-comments"></i>
+					<i class="fas fa-fw fa-money-bill-wave"></i>
 				</div>
 				<div class="mr-5"><?php echo $retornoReceita->getValor(); ?></div>
 			</div>
@@ -45,7 +60,7 @@ $retornoReceita = $repoReceita->somarReceita();
 		<div class="card text-white bg-warning o-hidden h-100">
 			<div class="card-body">
 				<div class="card-body-icon">
-					<i class="fas fa-fw fa-shopping-cart"></i>
+					<i class="fas fa-fw fa-wallet"></i>
 				</div>
 				<div class="mr-5"><?php echo $retornoDespesa->getValor(); ?></div>
 			</div>
@@ -58,15 +73,15 @@ $retornoReceita = $repoReceita->somarReceita();
 	</div>
 
 	<div class="col-xl-4 col-sm-6 mb-3">
-		<div class="card text-white bg-success o-hidden h-100">
+		<div class="card text-white bg-<?php echo $retornoCor;?> o-hidden h-100">
 			<div class="card-body">
 				<div class="card-body-icon">
-					<i class="fas fa-fw fa-life-ring"></i>
+					<i class="fas fa-fw fa-cash-register"></i>
 				</div>
-				<div class="mr-5"><?php echo $retornoReceita->getValor() - $retornoDespesa->getValor(); ?></div>
+				<div class="mr-5"><?php echo $retornoTotal; ?></div>
 			</div>
 			<a class="card-footer text-white clearfix small z-1" href="#"> <span
-				class="float-left">Caixa Total</span> <span class="float-right"> <i
+				class="float-left">Caixa</span> <span class="float-right"> <i
 					class="fas fa-angle-right"></i>
 			</span>
 			</a>
@@ -76,15 +91,31 @@ $retornoReceita = $repoReceita->somarReceita();
 
 <!-- Area Chart Example-->
 <div class="card mb-3">
-	<div class="card-header">
-		<i class="fas fa-chart-area"></i> Area Chart Example
-	</div>
-	<div class="card-body">
-		<canvas id="myAreaChart" width="100%" height="30"></canvas>
-	</div>
-	<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+  <div class="card-header">
+    <i class="fas fa-chart-area"></i>
+    Area Chart Example</div>
+  <div class="card-body">
+    <canvas id="myAreaChart" width="100%" height="30"></canvas>
+  </div>
+  <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
 </div>
 
+
+<?php 
+
+//$res = json_encode($array);
+
+$variavel1 = "121";
+$variavel2 = "12312";
+
+$res = "$variavel1, $variavel2, 12324, 123, 18287, 2131, 31274, 33259, 25849, 24159, 32651, 123213";
+
+echo $res;
+
+
+?>
+
+<input type="hidden" value="<?php echo $res;?>" id="dataSetChart"/>
 
 
 <?php
--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -4,13 +4,20 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart");
+var dataSetChart = document.getElementById("dataSetChart").value;
+var quebrado = dataSetChart.split(',');
+
+
+var frutas = [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984];
+
+
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: ["Janeiro 1", "Fevereiro 2", "Março 3", "Abril 4", "Maio 5", "Junho 6", "Julho 7", "Agosto 8", "Setembro 9", "Outubro 10", "Novembro 11", "Dezembro 12"],
     datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
+      //label: "Sessions",
+      //lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
       pointRadius: 5,
@@ -20,7 +27,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984],
+      data: quebrado,
     }],
   },
   options: {
